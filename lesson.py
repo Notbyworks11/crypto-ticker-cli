65614dd6915f78cd2f65b715a70e7e9c19c5c9f1
Notbyworks11 <Kwadwoappiah479@gmail.com>
2025-09-16 20:12:21 +0100

    updated

--- lesson.py
+++ lesson.py
@@ -1,3 +1,4 @@
+import sys
 def usd_to_gbp(usd, rate=0.78):
     return usd * rate
 
@@ -11,7 +12,21 @@ def pretty_usd(value):
 def pretty_gbp(value):
     """Format a number as GBP with £ and 2 decimals."""
     return f"£{value:,.2f}"
+def parse_flaot(s: str, label: str) -> float:
+    try:
+        return float(s)
+    except ValueError:
+        print(f"Error: {label} must be a number.Got {s!r}. ")
+        raise SystemExit(1)
 
 gbp_amount = 250
 amount_in_usd = gbp_to_usd(gbp_amount)
 print(f"{pretty_gbp(gbp_amount)} is {pretty_usd(amount_in_usd)}")
+
+
+def parse_float(s: str, label : str) -> float:
+    try:
+        return float(s)
+    except ValueError:
+        print(f'Error: {label} must be a number. Got {s!r}')
+        raise SystemExit(1)
